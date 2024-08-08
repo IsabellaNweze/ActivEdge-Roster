98de3991d42271b1d66abed49b1be9d2891fc053
htmlkill <110816510+htmlkill@users.noreply.github.com>
2024-08-08 12:54:42 +0100

    Third Commit

--- src/ActivEdge_Roster.java
+++ src/ActivEdge_Roster.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ActivEdge_Roster {
     public static void main(String[] args) {
-
-
+        ArrayList<String> workDays = new ArrayList<String>();
+        workDays.add("Monday");
+        workDays.add("Tuesday");
+        workDays.add("Wednesday");
+        workDays.add("Thursday");
+        workDays.add("Friday");
     }
+
 }
