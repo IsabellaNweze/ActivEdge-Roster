2427dfedfd88baa6abe6a1ce8df7573ac88445bb
htmlkill <110816510+htmlkill@users.noreply.github.com>
2024-08-08 13:33:33 +0100

    added arraylist

--- src/ActivEdge_Roster.java
+++ src/ActivEdge_Roster.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,42 +9,44 @@ import java.util.Map;
 public class ActivEdge_Roster {
     public static void main(String[] args) {
 
-        Map <Integer, String> Staff = new HashMap<>();
-        Staff.put(1,"Gbolahan Fatunde" );
-        Staff.put(2,"Ademola Babatunde" );
-        Staff.put(3,"Praise Akobundu" );
-        Staff.put(4,"Smart Agbawo" );
-        Staff.put(5,"Chioma Nwadozie" );
-        Staff.put(6,"Sodeeq Taiwo" );
-        Staff.put(7,"Collins Eze" );
-        Staff.put(8,"Delight Emmanuel" );
-        Staff.put(9,"Adewale Favour" );
-        Staff.put(10,"Hammed Mudashir" );
-        Staff.put(11,"Thompson Idowu" );
-        Staff.put(12,"Ekwugha Elochukwu" );
-        Staff.put(13,"Salako Akinbolade" );
-        Staff.put(14,"Ichebadu Chukwu" );
-        Staff.put(15,"Ajah Kenneth" );
-        Staff.put(16,"Onakoya Kayode" );
-        Staff.put(17,"Jennifer Ewuzie" );
-        Staff.put(18,"Miracle Shaibu" );
-        Staff.put(19,"Christian Aka" );
-        Staff.put(20,"Amietubodie Otonye " );
+        Map<Integer, String> Staff = new HashMap<>();
+        Staff.put(1, "Gbolahan Fatunde");
+        Staff.put(2, "Ademola Babatunde");
+        Staff.put(3, "Praise Akobundu");
+        Staff.put(4, "Smart Agbawo");
+        Staff.put(5, "Chioma Nwadozie");
+        Staff.put(6, "Sodeeq Taiwo");
+        Staff.put(7, "Collins Eze");
+        Staff.put(8, "Delight Emmanuel");
+        Staff.put(9, "Adewale Favour");
+        Staff.put(10, "Hammed Mudashir");
+        Staff.put(11, "Thompson Idowu");
+        Staff.put(12, "Ekwugha Elochukwu");
+        Staff.put(13, "Salako Akinbolade");
+        Staff.put(14, "Ichebadu Chukwu");
+        Staff.put(15, "Ajah Kenneth");
+        Staff.put(16, "Onakoya Kayode");
+        Staff.put(17, "Jennifer Ewuzie");
+        Staff.put(18, "Miracle Shaibu");
+        Staff.put(19, "Christian Aka");
+        Staff.put(20, "Amietubodie Otonye ");
 
-        Map <Integer, String> Interns = new HashMap<>();
+        Map<Integer, String> Interns = new HashMap<>();
 
-        Interns.put(21,"Siyaka Promise");
-        Interns.put(22,"Kanu Kennedy");
-        Interns.put(23,"Godspower Amun");
-        Interns.put(24,"Praise Adepoju");
-        Interns.put(25,"Nweze Isabella");
-        Interns.put(26,"John Ozeigbe");
-        Interns.put(27,"Anthony Obinugwu");
-
-
+        Interns.put(21, "Siyaka Promise");
+        Interns.put(22, "Kanu Kennedy");
+        Interns.put(23, "Godspower Amun");
+        Interns.put(24, "Praise Adepoju");
+        Interns.put(25, "Nweze Isabella");
+        Interns.put(26, "John Ozeigbe");
+        Interns.put(27, "Anthony Obinugwu");
 
 
-
+        ArrayList<String> workDays = new ArrayList<>();
+        workDays.add("Monday");
+        workDays.add("Tuesday");
+        workDays.add("Wednesday");
+        workDays.add("Thursday");
+        workDays.add("Friday");
     }
-
 }
