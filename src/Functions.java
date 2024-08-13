edaab995f54ddb94caf5154bf46d01a76c0e2e19
htmlkill <110816510+htmlkill@users.noreply.github.com>
2024-08-13 14:43:43 +0100

    Resolved Conflicts

--- src/Functions.java
+++ src/Functions.java
@@ -7,11 +7,6 @@ import java.util.Map;
 public interface Functions { // Created an interface to handle functions class
     public void  StaffAssign(Map<Integer,String> map, ArrayList<String> workdays);
     public void InternAssign(Map<Integer,String> map,ArrayList<String> days);
-<<<<<<< HEAD
     public void addStaffMember(HashMap<Integer, String> Staff, ArrayList<String> workdays);
     public void addNewIntern(HashMap<Integer, String> Staff, ArrayList<String> workdays);
-=======
 
-
->>>>>>> 64253e5cab5f76087c3bfc73c5d68c0e2438ed64
-}
