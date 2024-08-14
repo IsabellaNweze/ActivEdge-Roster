0bf423b5e0d35d3cd98fcc41d0d9d879fd5e1c71
htmlkill <110816510+htmlkill@users.noreply.github.com>
2024-08-14 16:59:32 +0100

    Roster update for corpers

--- src/ActivEdge_Roster.java
+++ src/ActivEdge_Roster.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class ActivEdge_Roster extends JFrame{
+public class ActivEdge_Roster {
     public static void main(String[] args)  {
 
         Functions function = new Functions_Impl(); // Assigning the functions_impl (Functions implementation) class to a new name called functions
